b9165f052d3777b7d0de5320a7d903166d579d4c
Daniel Allspach <dj26alls@siena.edu>
2019-12-15 01:46:37 -0500

    Update findingChart.py
    Removed the useage of raw input. Now the file can be excuted in the command line with massive scrawls of interactive text. The file is also more streamlined.

--- findingChart.py
+++ findingChart.py
@@ -1,132 +1,133 @@
- #Importing packages
-import numpy as np 
-import matplotlib.pyplot as plt 
+# To Do List ----------------------------------------------------------------------------
+
+'''
+- Turn rotation and footprint masks into loops 
+
+- Think of a better way to set up rotation 
+
+
+'''
+
+
+# Import Libraries ----------------------------------------------------------------------
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
 from astroplan.plots import plot_finder_image
 from astroplan.plots import plot_sky
 from astroplan import FixedTarget
+
 from astropy.coordinates import SkyCoord
 import astropy.units as u
-import matplotlib.patches as patches 
+
+import argparse
+
+# Setting Up Command Line Arguments -----------------------------------------------------
+
+parser = argparse.ArgumentParser()
+
+# Positional Argument
+
+parser.add_argument( "tpos", type = list,
+    help =" Target coordinates of the finding image in degrees: [ra,dec]")
+
+# Optional Arguments
+
+parser.add_argument( "-fov", "--fieldOfView", type = int, default = 0.4,
+    help = " Field of view in degrees. Defaults to 0.4")
+
+parser.add_argument( "-off", "--offset", type = list, default = [0,0],
+    help = " Vector distance from the center of the finding image to the target position in arcminutes. defaults to [ra=0,dec=0]")
+
+parser.add_argument( "-r", "--rotate", type =float, default = 0,
+    help = " Angle to rotate the image from north. Defaults to north at 0 degrees.")
+
+parser.add_argument( "-tn","--targetName", type = str, default = 'Target',
+    help = "Name of the target object")
+
+parser.add_argument( "-v", "--verbose", action = 'store_true', default = False,
+    help = " Program will talk while running")
+
+# Description in help
+
+args = parser.parse_args()
 
 
-'''
-#################################################################################
---------------------------------------------------------------------------------
-################################################################################
-Generating a Finding chart for the Breyo Observatory:
+#description = '''Generates a finding chart for an target celestial object
+#   for the Breyo Observatory's SBIG stl-11000m CCD.'''
 
-    - Current version for the SBIG STL-11000M ccd ONLY!
-    - required packages:
-        - Numpy
-        - Astropy
-        - Matplotlib
-        - scipy
-        - astroplan  (requires visual C++ build tools)
-        - astroquery (requires visual C++ build tools)
-    - Functionality:
-        - generates a finding chart
-    - useage:
-        - open an python environment terminal (ie anaconda prompt)
-        - move to directory with this folder in it
-        - type %run findingChart.py
-        - follow the directions appearing in the terminal 
-    - To do:
-        - improve fact list
-    - Future programs: 
-        - full sky map 
-        - airmass chart
-        - utilize argparse (write help) 
-        - overarching program that runs everything at once
+# Unloading Arguments -------------------------------------------------------------------
 
-################################################################################
---------------------------------------------------------------------------------
-################################################################################
-'''
-print('---------------------------------------------')
-targetname = input('Please input the name of your target:')
-print('---------------------------------------------')
-print('')
-print('')
-# angular size/initial conditions of the rectangles 
+target = args.tpos
 
-#main imaging ccd
+fov = args.fieldOfView
+
+off = args.offset
+
+rotate = args.rotate
+
+targetName = args.targetName
+
+verbose = args.verbose
+
+# Unpacking
+
+tra = float(target[0])
+tdec = float(target[1])
+
+offra = float(off[0])
+offdec = float(off[1])
+
+# Defining CCD Shapes -------------------------------------------------------------------
+
+#main ccd
+
+if verbose == True:
+    print("Defining the shape of the main ccd's sky footprint.")
+    
 imcordx = np.array([0.25,0.25,0.55,0.55,0.4])
 imcordy = np.array([0.17833335000000003,0.62166665,0.62166665,0.17833335000000003,0.4])
 
 #guiding ccd
+
+if verbose == True:
+    print("Defining the shape of the guide ccd's sky footprint.")
+   
 gcordx = np.array([0.12,0.12,0.18,0.18,0.16])
 gcordy = np.array([0.38, 0.42, 0.42, 0.38,0.4])
 
-#field of view of targeted space map
-print('---------------------------------------------')
-print('Please put Finding Chart field of view radius.')
-print('If you are unsure please enter 0.4.')
-print('')
-fov = float(input( "Finding chart field of view radius in degrees:"))
-print('---------------------------------------------')
-print('')
-print('')
-# for debugging -------------------------(comment out when done)--------
-#fov = 0.4
-# -------------------------------------------------------------------
+# degrees per pixel
 
+if verbose == True:
+    print("Determining image scale.")
+    
 degpix = fov/150
 
-print('---------------------------------------------')
-print('Please put in the target Coordinates below.')
-print('')
-tra = float(input( "Target's Right Ascension in degrees:"))
-tdec = float(input("Target's Declination in degrees:"))
-print('---------------------------------------------')
-print('')
-print('')
-# for debugging -------------------------(comment out when done)--------
-#tra = 267.9920073160415
-#tdec = 37.55208262984203
-# -------------------------------------------------------------------
-
-print('---------------------------------------------')
-print('For the offset settings if you desire no offset at all input a 0')
-print('Leaving these blank will BREAK the program!!!')
-print('For moving the image to the left please make the offset RA positive.')
-print('For moving the image to the right please make the offset RA negative.')
-print('For moving the image to up please make the offset DEC positive.')
-print('For moving the image to down please make the offset DEC negative.')
-print('---------------------------------------------')
-print('')
-offra = float(input( "Right Ascension offset in arcminutes:"))
-offdec = float(input("Declination offset in arcminutes:"))
-print('---------------------------------------------')
-print('')
-print('')
-#for debugging -------------------------(comment out when done)--------
-#offra = 3
-#offdec = 0.3
-# -------------------------------------------------------------------
-
-offra = (offra*u.arcminute).to(u.deg)/u.deg
-offdec = (offdec*u.arcminute).to(u.deg)/u.deg
-
-tra = tra + offra
-tdec = tdec +offdec
-
-#rotate field
-print('---------------------------------------------')
-print('Please input a rotation angle. If you do not want a rotation angle')
-print('please enter a 0 below')
-print('---------------------------------------------')
-print('')
-rotate = float(input('Rotation angle in degrees:'))
-print('---------------------------------------------')
-print('')
-print('')
-
-    
 imcordx = ((imcordx - 0.4)/degpix)
 imcordy = ((imcordy - 0.4)/degpix)
 gimcordx = ((gcordx - 0.4)/degpix)
 gimcordy = ((gcordy - 0.4)/degpix)
 
+# Offset --------------------------------------------------------------------------------
+
+if verbose == True:
+    print('Offsetting image coordinates form target coordinates.')
+    
+    
+offra = (offra*u.arcminute).to(u.deg)/u.deg
+offdec = (offdec*u.arcminute).to(u.deg)/u.deg
+
+cra = tra + offra
+cdec = tdec +offdec
+
+# Rotate Field --------------------------------------------------------------------------
+if verbose == True and not rotate ==0 :
+    print("Rotating orientation of ccd footprints on the sky.")
+    
+    
 imangle = (np.arctan(imcordy[0:4]/imcordx[0:4])*u.rad).to(u.deg)
 gimangle = (np.arctan(gimcordy[0:4]/gimcordx[0:4])*u.rad).to(u.deg)
 imrad = imcordy[0:4]/np.sin(imangle.to(u.rad))
@@ -142,19 +143,34 @@ imcordy = imrad*np.sin(imangle)
 gimcordx = gimrad*np.cos(gimangle)
 gimcordy = gimrad*np.sin(gimangle)
 
-# setting up for ploting 
-center_coord = SkyCoord(ra = tra*u.deg, dec = tdec*u.deg)
-center = FixedTarget(coord=center_coord, name=targetname)
+# Pulling in Sky Coords -----------------------------------------------------------------
+
+if verbose == True :
+    print("Rotating orientation of ccd footprints on the sky.")
+       
+center_coord = SkyCoord(ra = cra*u.deg, dec = cdec*u.deg)
+center = FixedTarget(coord=center_coord, name=targetName+' Finding Chart')
+
+# Translation ---------------------------------------------------------------------------
 
 imcordx = imcordx + 149.5
 imcordy = imcordy + 149.5
 gimcordx = gimcordx + 149.5
 gimcordy = gimcordy + 149.5
-  
-plt.figure()
-ax,hdu = plot_finder_image(center,fov_radius = fov*u.deg)
 
+# Plotting ------------------------------------------------------------------------------
+if verbose == True :
+    print("Pulling star field for finding chart from DSS.")
+
+
+plt.figure()
+ax, hdu = plot_finder_image(center,fov_radius = fov*u.deg)
+    
+
+if verbose == True :
+    print("Creating footprint masks.")
 #box for imaging ccd:
+
 line1 = patches.FancyArrow(imcordx[0],imcordy[0],imcordx[1]-imcordx[0],imcordy[1]-imcordy[0],color='blue')
 line2 = patches.FancyArrow(imcordx[1],imcordy[1],imcordx[2]-imcordx[1],imcordy[2]-imcordy[1],color='blue')
 line3 = patches.FancyArrow(imcordx[2],imcordy[2],imcordx[3]-imcordx[2],imcordy[3]-imcordy[2],color='blue')
@@ -165,7 +181,8 @@ ax.add_patch(line2)
 ax.add_patch(line3)
 ax.add_patch(line4)
 
-#box for guiding ccd: 
+#box for guiding ccd:
+
 gline1 = patches.FancyArrow(gimcordx[0],gimcordy[0],gimcordx[1]-gimcordx[0],gimcordy[1]-gimcordy[0],color='red')
 gline2 = patches.FancyArrow(gimcordx[1],gimcordy[1],gimcordx[2]-gimcordx[1],gimcordy[2]-gimcordy[1],color='red')
 gline3 = patches.FancyArrow(gimcordx[2],gimcordy[2],gimcordx[3]-gimcordx[2],gimcordy[3]-gimcordy[2],color='red')
@@ -177,19 +194,19 @@ ax.add_patch(gline3)
 ax.add_patch(gline4)
 
 # circle target
-circle = patches.Circle((149.5+(offra/degpix),149.5+(offdec/degpix)),radius=4,fill = False, color ='green',label='target')
+
+circle = patches.Circle((149.5+(offra/degpix),149.5+(offdec/degpix)),
+        radius=4,fill = False, color ='green',label=targetName)
+
 ax.add_patch(circle)
 
 # Legend
+
 plt.legend(handles=[circle,line4,gline4])
+
+# Generate plot
+
 plt.show()
 
-# just one more thing
-#fact list:
-print('---------------------------------------------')
-print('Fact List: (WIP)')
-print('---------------------------------------------')
-print('The center RA of this finding image is %(ra)f degrees' %{'ra': tra})
-print('The center DEC of this finding image is %(dec)f degrees' %{'dec': tdec})
-# print('Set the rotator angle to: %(r)f' %{'r': (rotate+ 0)}) #This needs calibration
-print('---------------------------------------------')
+# finnished -----------------------------------------------------------------------------
+
